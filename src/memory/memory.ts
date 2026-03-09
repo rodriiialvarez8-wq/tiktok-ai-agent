4670c1155744b5f110a529c65fee776c58748529
rodriiialvarez8-wq <rodriiialvarez8@gmail.com>
2026-03-09 18:56:00 +0100

    Update memory.ts

--- src/memory/memory.ts
+++ src/memory/memory.ts
@@ -1,1 +1,27 @@
 Create memory.ts
+export interface VideoMemory {
+  title: string;
+  views: number;
+  likes: number;
+  comments: number;
+  date: string;
+}
+
+export class MemoryStore {
+
+  private videos: VideoMemory[] = [];
+
+  saveVideo(video: VideoMemory) {
+    this.videos.push(video);
+    console.log("Video guardado en memoria:", video.title);
+  }
+
+  getTopVideos() {
+    return this.videos.sort((a, b) => b.views - a.views).slice(0, 5);
+  }
+
+  getAllVideos() {
+    return this.videos;
+  }
+
+}
